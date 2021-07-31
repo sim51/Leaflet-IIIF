e77cd6212de0f0693b3ba71ae955757174329c0f
Benoit Simard <contact@bsimard.com>
2021-07-31 16:14:37 +0200

    Review ognore files

--- lib/esm/index.d.ts
+++
@@ -1,4 +0,0 @@
-import { IIIFLayerOptions, IIIFControlOptions, ServerCapabilities } from "./types";
-import { IIIFLayer } from "./layer";
-import { IIIFControl } from "./toolbar";
-export { IIIFLayer, IIIFControl, IIIFLayerOptions, IIIFControlOptions, ServerCapabilities };
