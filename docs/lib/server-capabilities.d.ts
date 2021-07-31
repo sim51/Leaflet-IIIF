6617ffe3f9a470d104d63fdc1fbcc58d6e92c1fe
Benoit Simard <contact@bsimard.com>
2021-07-31 15:00:49 +0200

    [site] Correct site generation for example

--- docs/lib/server-capabilities.d.ts
+++
@@ -1,3 +0,0 @@
-import { ServerCapabilities } from "../types";
-export declare function computeServerCapabilities(data: any): ServerCapabilities;
-export declare function computeServerCapabilitiesForV1(data: any): ServerCapabilities;
